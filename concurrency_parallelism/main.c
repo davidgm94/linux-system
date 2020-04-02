3200ff706f89264e24c3f8fdf1b2ee64d43b546b
David <davidgonzalezmartin94@gmail.com>
2020-04-03 01:10:19 +0200

    First threads...

--- concurrency_parallelism/main.c
+++ concurrency_parallelism/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pthread.h>
 
+void* foo(void* number) {
+	printf("%d\n", rand());
+	return NULL;
+}
+
+#define THREAD_COUNT 8
 int main(int argc, char* argv[])
 {
-	printf("Hello world\n");
+	pthread_t threadPool[THREAD_COUNT];
+	for (int i = 0; i < THREAD_COUNT; i++)
+		pthread_create(&threadPool[i], 0, foo, 0);
 }
