16a4f0e03d96b8b84bc2eeb354abe5ba63f298f0
David <davidgonzalezmartin94@gmail.com>
2020-04-04 01:10:03 +0200

    New project structure

--- pthread/main.c
+++ pthread/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
-int main(void)
+#include <pthread.h>
+#include "common.h"
+#define THREAD_COUNT 8
+
+typedef struct
 {
-	printf("Hello world\n");
+	unsigned index;
+} thread_data;
+
+void* fn(void* args)
+{
+	thread_data* data = (thread_data*) args;
+	unsigned index = data->index;
+	return (void*)printf("Hello, this is thread no %u\n", index);
+}
+
+int main(int argc, char* argv[])
+{
+	i32 s = 0;
+	pthread_t thread_pool[THREAD_COUNT];
+	thread_data thread_pool_data[THREAD_COUNT];
+	for (int i = 0; i < THREAD_COUNT; i++)
+	{
+		pthread_t* thread = &thread_pool[i];
+		thread_pool_data[i].index = i;
+		void* args = &thread_pool_data[i];
+		pthread_create(thread, NULL, fn, args);
+	}
+
+	for (int i = 0; i < THREAD_COUNT; i++)
+	{
+		pthread_t thread = thread_pool[i];
+		pthread_join(thread, NULL);
+	}
+
 	return 0;
 }
